97de90fdee7f8196b82ac162ce4a46082a3d48e0
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-03-03 03:16:51 +0900

    user_controllerの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create!(user_params)
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update!(user_params)
+  end
+
+  private
+
+  def user_params
+    params.required(:users).permit(:name)
+  end
+end
