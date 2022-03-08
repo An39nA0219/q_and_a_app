1611d3bc8e707f9575ea3bc4f514950ec37bd9ed
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-03-09 01:52:42 +0900

    バリデーションの作成

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,8 @@
 class Question < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :is_solved, presence: true
+
   belongs_to :user
   has_many :answers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
   before_save { self.email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true
 
   has_many :questions
   has_many :answers, foreign_key: :answerer_id
