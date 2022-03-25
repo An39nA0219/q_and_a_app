2094519cc31426c825662e7b1d9de0478b0b85c7
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-03-25 21:02:23 +0900

    fix #6 order時のソート順の指定をシンボル化

--- app/controllers/admins/questions_controller.rb
+++ app/controllers/admins/questions_controller.rb
@@ -3,27 +3,27 @@ class Admins::QuestionsController < ApplicationController
 
   def index
     questions = if !!params[:words]
-                  Question.where('title LIKE ?', "%#{params[:words]}%").order(created_at: 'desc')
+                  Question.where('title LIKE ?', "%#{params[:words]}%").order(created_at: :desc)
                 else
-                  Question.all.order(created_at: 'desc')
+                  Question.all.order(created_at: :desc)
                 end
     @questions = questions.page(params[:page]).per(10)
   end
 
   def solved
     questions = if !!params[:words]
-                  Question.where(solved: true).where('title LIKE ?', "%#{params[:words]}%").order(created_at: 'desc')
+                  Question.where(solved: true).where('title LIKE ?', "%#{params[:words]}%").order(created_at: :desc)
                 else
-                  Question.where(solved: true).order(created_at: 'desc')
+                  Question.where(solved: true).order(created_at: :desc)
                 end
     @questions = questions.page(params[:page]).per(10)
   end
 
   def unsolved
     questions = if !!params[:words]
-                  Question.where(solved: false).where('title LIKE ?', "%#{params[:words]}%").order(created_at: 'desc')
+                  Question.where(solved: false).where('title LIKE ?', "%#{params[:words]}%").order(created_at: :desc)
                 else
-                  Question.where(solved: false).order(created_at: 'desc')
+                  Question.where(solved: false).order(created_at: :desc)
                 end
     @questions = questions.page(params[:page]).per(10)
   end
